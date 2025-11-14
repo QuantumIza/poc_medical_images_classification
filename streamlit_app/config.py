f370bd692ff329ed596a088448a287108c86cba5
QuantumIza <farizadah@gmail.com>
2025-11-14 22:55:21 +0100

    Mise à jour des accès HuggingFace

--- streamlit_app/config.py
+++ streamlit_app/config.py
@@ -1,50 +1,44 @@
-# ------------------------------
-# CONFIGURATION RESSOURCES HF
-# ------------------------------
+# ------------------------------------------------------- #
+# --- ACCES AUX RESSOURCES STOCKEES DANS HUGGING FACE --- #
+# ------------------------------------------------------- #
 
 HF_RESOURCES = {
     "models": {
-        "cnn": {
-            "local": "outputs/models/cnn_model.h5",
-            "url": "https://huggingface.co/QuantumIza/poc-baseline-cnn/resolve/main/models/cnn_model.h5"
+        "baseline_cnn": {
+            "local": "src/outputs/baseline_cnn/model/best_model_baseline_cnn.keras",
+            "url": "https://huggingface.co/QuantumIza/poc-baseline-cnn/resolve/main/baseline_cnn/model/best_model_baseline_cnn.keras"
         },
-        "ictn": {
-            "local": "outputs/models/ictn_model.h5",
-            "url": "https://huggingface.co/QuantumIza/poc-baseline-cnn/resolve/main/models/ictn_model.h5"
-        }
-    },
-    "labels": {
-        "cnn": {
-            "local": "outputs/labels/class_labels_cnn.json",
-            "url": "https://huggingface.co/QuantumIza/poc-baseline-cnn/resolve/main/labels/class_labels_cnn.json"
+        "icnt": {
+            "local": "src/outputs/icnt/model/best_model_icnt.keras",
+            "url": "https://huggingface.co/QuantumIza/poc-baseline-cnn/resolve/main/icnt/model/best_model_icnt.keras"
         },
-        "ictn": {
-            "local": "outputs/labels/class_labels_icnt.json",
-            "url": "https://huggingface.co/QuantumIza/poc-baseline-cnn/resolve/main/labels/class_labels_icnt.json"
+        "iiv3": {
+            "local": "src/outputs/iiv3/model/best_model_iiv3.keras",
+            "url": "https://huggingface.co/QuantumIza/poc-baseline-cnn/resolve/main/iiv3/model/best_model_iiv3.keras"
         }
     },
     "datasets": {
-        "stats": {
-            "local": "outputs/eda/dataset_stats.csv",
-            "url": "https://huggingface.co/QuantumIza/poc-baseline-cnn/resolve/main/eda/dataset_stats.csv"
-        },
         "full": {
-            "local": "outputs/data/full_dataset.csv",
-            "url": "https://huggingface.co/QuantumIza/poc-baseline-cnn/resolve/main/data/full_dataset.csv"
+            "local": "src/outputs/eda/df_full.csv",
+            "url": "https://huggingface.co/QuantumIza/poc-baseline-cnn/resolve/main/eda/df_full.csv"
         },
         "sample": {
-            "local": "outputs/data/sample_dataset.csv",
-            "url": "https://huggingface.co/QuantumIza/poc-baseline-cnn/resolve/main/data/sample_dataset.csv"
+            "local": "data/my_df_sample.csv",
+            "url": "https://huggingface.co/QuantumIza/poc-baseline-cnn/resolve/main/df_sample.csv"
         }
     },
     "history": {
-        "cnn": {
-            "local": "outputs/history/history_cnn.json",
-            "url": "https://huggingface.co/QuantumIza/poc-baseline-cnn/resolve/main/history/history_cnn.json"
+        "baseline_cnn": {
+            "local": "src/outputs/baseline_cnn/history/history_baseline_cnn.json",
+            "url": "https://huggingface.co/QuantumIza/poc-baseline-cnn/resolve/main/baseline_cnn/history/history_baseline_cnn.json"
         },
-        "ictn": {
-            "local": "outputs/history/history_icnt.json",
-            "url": "https://huggingface.co/QuantumIza/poc-baseline-cnn/resolve/main/history/history_icnt.json"
+        "icnt": {
+            "local": "src/outputs/icnt/history/history_icnt.json",
+            "url": "https://huggingface.co/QuantumIza/poc-baseline-cnn/resolve/main/icnt/history/history_icnt.json"
+        },
+        "iiv3": {
+            "local": "src/outputs/iiv3/history/history_iiv3.json",
+            "url": "https://huggingface.co/QuantumIza/poc-baseline-cnn/resolve/main/iiv3/history/history_iiv3.json"
         }
     }
 }
