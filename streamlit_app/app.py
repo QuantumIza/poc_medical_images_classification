64ae799f8aa2105e9f918103a87eb4fad2976ee0
QuantumIza <farizadah@gmail.com>
2025-11-15 00:05:33 +0100

    syntaxe

--- streamlit_app/app.py
+++ streamlit_app/app.py
@@ -24,7 +24,7 @@ from loaders import (
     load_image_from_url,
     preprocess_image_cnn,
     preprocess_image_icnt,
-    preprocess_image_iiv3
+    preprocess_image_iiv3,
     load_dataset_stats
 )
 
