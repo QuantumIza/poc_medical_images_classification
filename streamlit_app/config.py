40c6028a5e63c0b551be39b82b3aeb3275e05223
QuantumIza <farizadah@gmail.com>
2025-11-15 04:34:15 +0100

    Ajout urls liées aux ressources alimentant onglet Performances

--- streamlit_app/config.py
+++ streamlit_app/config.py
@@ -46,3 +46,39 @@ HF_RESOURCES = {
         }
     }
 }
+HF_PERFORMANCES = {
+    "baseline_cnn": {
+        "learning_curves": "https://huggingface.co/QuantumIza/poc-baseline-cnn/resolve/main/baseline_cnn/assets/learning_curves_loss_cnn.png",
+        "confusion_matrix": "https://huggingface.co/QuantumIza/poc-baseline-cnn/resolve/main/baseline_cnn/assets/matrice_confusion_cnn.png",
+        "classification_report": "https://huggingface.co/QuantumIza/poc-baseline-cnn/resolve/main/baseline_cnn/test_evaluation/classification_report_cnn.csv",
+        "roc_curve": "https://huggingface.co/QuantumIza/poc-baseline-cnn/resolve/main/baseline_cnn/assets/roc_multiclasse_cnn.png",
+        "metrics": "https://huggingface.co/QuantumIza/poc-baseline-cnn/resolve/main/baseline_cnn/metrics/metrics_baseline_cnn.csv",
+        "summary": "https://huggingface.co/QuantumIza/poc-baseline-cnn/resolve/main/baseline_cnn/summary/summary_baseline_cnn.csv",
+        "pca": "https://huggingface.co/QuantumIza/poc-baseline-cnn/resolve/main/baseline_cnn/assets/pca_3d_interactif_light_cnn.html",
+        "gradcam_success": "https://huggingface.co/QuantumIza/poc-baseline-cnn/resolve/main/baseline_cnn/convincing_cases/gradcam_convincing_malignant_conf_1.00_idx_129.png",
+        "gradcam_error": "https://huggingface.co/QuantumIza/poc-baseline-cnn/resolve/main/baseline_cnn/critical_errors/gradcam_error_malignant_to_benign_true_malignant_pred_benign_conf_0.95_idx_7234.png"
+    },
+    "icnt": {
+        "learning_curves": "https://huggingface.co/QuantumIza/poc-baseline-cnn/resolve/main/icnt/assets/learning_curves_loss_cnn.png",
+        "confusion_matrix": "https://huggingface.co/QuantumIza/poc-baseline-cnn/resolve/main/icnt/assets/matrice_confusion_icnt.png",
+        "classification_report": "https://huggingface.co/QuantumIza/poc-baseline-cnn/resolve/main/icnt/test_evaluation/classification_report_icnt.csv",
+        "roc_curve": "https://huggingface.co/QuantumIza/poc-baseline-cnn/resolve/main/icnt/assets/roc_multiclasse_icnt.png",
+        "metrics": "https://huggingface.co/QuantumIza/poc-baseline-cnn/resolve/main/icnt/metrics/metrics_icnt.csv",
+        "summary": "https://huggingface.co/QuantumIza/poc-baseline-cnn/resolve/main/icnt/summary/summary_modele_icnt.csv",
+        "pca": "https://huggingface.co/QuantumIza/poc-baseline-cnn/resolve/main/icnt/assets/pca_3d_interactif_light_icnt.html",
+        "gradcam_success": "https://huggingface.co/QuantumIza/poc-baseline-cnn/resolve/main/icnt/convincing_cases/gradcam_convincing_malignant_conf_1.00_idx_106.png",
+        "gradcam_error": "https://huggingface.co/QuantumIza/poc-baseline-cnn/resolve/main/icnt/critical_errors/gradcam_error_malignant_to_normal_true_malignant_pred_normal_conf_1.00_idx_5847.png"
+    },
+    "iiv3": {
+        "learning_curves": "https://huggingface.co/QuantumIza/poc-baseline-cnn/resolve/main/iiv3/assets/learning_curves_loss_iiv3.png",
+        "confusion_matrix": "https://huggingface.co/QuantumIza/poc-baseline-cnn/resolve/main/iiv3/assets/matrice_confusion_iiv3.png",
+        "classification_report": "https://huggingface.co/QuantumIza/poc-baseline-cnn/resolve/main/iiv3/test_evaluation/classification_report_iiv3.csv",
+        "roc_curve": "https://huggingface.co/QuantumIza/poc-baseline-cnn/resolve/main/iiv3/assets/roc_multiclasse_iiv3.png",
+        "metrics": "https://huggingface.co/QuantumIza/poc-baseline-cnn/resolve/main/iiv3/metrics/metrics_iiv3.csv",
+        "summary": "https://huggingface.co/QuantumIza/poc-baseline-cnn/resolve/main/iiv3/summary/summary_modele_iiv3_structured.csv",
+        "pca": "https://huggingface.co/QuantumIza/poc-baseline-cnn/resolve/main/iiv3/assets/pca_3d_interactif_light_iiv3.html",
+        "gradcam_success": "https://huggingface.co/QuantumIza/poc-baseline-cnn/resolve/main/iiv3/convincing_cases/gradcam_convincing_malignant_conf_1.00_idx_8.png",
+        "gradcam_error": "https://huggingface.co/QuantumIza/poc-baseline-cnn/resolve/main/iiv3/critical_errors/gradcam_error_malignant_to_benign_true_malignant_pred_benign_conf_0.99_idx_5400.png"
+    }
+}
+
