1fdee9a327d155a65308814cd2543655f0050830
QuantumIza <farizadah@gmail.com>
2025-11-17 09:46:37 +0100

    Update app.py

--- streamlit_app/app.py
+++ streamlit_app/app.py
@@ -667,13 +667,13 @@ with tab4:
     #     st.components.v1.html(requests.get(res["pca"]).text, height=450)
     col1, col2, col3 = st.columns(3)
     with col1:
-        st.components.v1.html(requests.get(res["pca"]).text, height=450)
+        st.components.v1.html(requests.get(res["pca"]).text)
     
     with col2:
-        st.components.v1.html(requests.get(res["pca"]).text, height=450)
+        st.components.v1.html(requests.get(res["pca"]).text)
     
     with col3:
-        st.components.v1.html(requests.get(res["pca"]).text, height=450)
+        st.components.v1.html(requests.get(res["pca"]).text)
     # -----------------------------------------------------
     # --- Bloc 5 : EXPLICABILITE
     # --------------------------------------------------------    
