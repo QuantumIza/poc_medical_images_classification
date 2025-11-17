996369e3bff3633597925ecade085dbe0e27d387
QuantumIza <farizadah@gmail.com>
2025-11-17 11:37:23 +0100

    Update app.py

--- streamlit_app/app.py
+++ streamlit_app/app.py
@@ -667,22 +667,46 @@ with tab4:
     #     st.components.v1.html(requests.get(res["pca"]).text, height=450)
     # Ligne 1
     row1_col1, row1_col2 = st.columns(2)
+    # with row1_col1:
+    #     st.components.v1.html(requests.get(res["pca"]).text, height=500)
+    #     st.markdown(
+    #     """
+    #     <div style="font-size:16px; font-weight:600; color:#005A9C; margin-bottom:4px;">
+    #         Vue A
+    #     </div>
+    #     <div style="font-size:16px; color:#444; margin-top:0;">
+    #         <strong>LEGENDE :</strong> 
+    #         <span style="color:blue;">Benign</span>, 
+    #         <span style="color:orange;">Malignant</span>, 
+    #         <span style="color:green;">Normal</span>
+    #     </div>
+    #     """,
+    #     unsafe_allow_html=True
+    # )
     with row1_col1:
+        st.markdown(
+            """
+            <div style="border:1px solid #ccc; border-radius:8px; padding:10px; margin-bottom:16px;">
+                <div style="font-size:16px; font-weight:600; color:#005A9C; margin-bottom:4px;">
+                    Vue A
+                </div>
+            """,
+            unsafe_allow_html=True
+        )
         st.components.v1.html(requests.get(res["pca"]).text, height=500)
         st.markdown(
-        """
-        <div style="font-size:16px; font-weight:600; color:#005A9C; margin-bottom:4px;">
-            Vue A
-        </div>
-        <div style="font-size:16px; color:#444; margin-top:0;">
-            <strong>LEGENDE :</strong> 
-            <span style="color:blue;">Benign</span>, 
-            <span style="color:orange;">Malignant</span>, 
-            <span style="color:green;">Normal</span>
-        </div>
-        """,
-        unsafe_allow_html=True
-    )
+            """
+            <div style="font-size:15px; color:#444; margin-top:6px;">
+                <strong>Légende :</strong> 
+                <span style="color:blue;">Benign</span>, 
+                <span style="color:orange;">Malignant</span>, 
+                <span style="color:green;">Normal</span>
+            </div>
+            </div>
+            """,
+            unsafe_allow_html=True
+        )
+
 
     with row1_col2:
         st.components.v1.html(requests.get(res["pca"]).text, height=500)
