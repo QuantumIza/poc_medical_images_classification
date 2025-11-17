34fc0a42c6f37cbc24e684ec494e6d14748ee744
QuantumIza <farizadah@gmail.com>
2025-11-17 11:30:04 +0100

    Update app.py

--- streamlit_app/app.py
+++ streamlit_app/app.py
@@ -668,6 +668,7 @@ with tab4:
     # Ligne 1
     row1_col1, row1_col2 = st.columns(2)
     with row1_col1:
+        st.components.v1.html(requests.get(res["pca"]).text, height=500)
         st.markdown(
         """
         <div style="font-size:16px; font-weight:600; color:#005A9C; margin-bottom:4px;">
